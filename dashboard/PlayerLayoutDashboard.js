d312fe5182ba91a888167f4135bd489d08f4c043
zoton2 <zoton2@gmail.com>
2017-06-11 00:05:55 +0100

    Small "workaround" on the Team Layout panel
    If there are more than 1 teams (a team can just be 1 player in a race),
    the panel is the same as before, but if there is only 1 team, the panel
    changes to sorting of that team's members; useful for co-op runs with
    more than 1 screen. I don't plan to keep it this way forever, but it
    could help out. (Ignore the copy/pasted code!)

--- dashboard/PlayerLayoutDashboard.js
+++ dashboard/PlayerLayoutDashboard.js
@@ -1,49 +1,86 @@
+// Lots of copy/pasted functions in this file, this implementation isn't permenant... hopefully.
+
 'use strict';
-$(function () {
-// Replicant initialization
-    var runDataActiveRunReplicant = nodecg.Replicant("runDataActiveRun");
-    runDataActiveRunReplicant.on("change", function (newValue, oldValue) {
-        if (typeof newValue !== 'undefined' && newValue != '') {
-            var names = new Array();
-           playerLayout_UpdateLayoutPanelWithTeams(newValue.teams);
-
-        }
-    });
-
-    function playerLayout_UpdateLayoutPanelWithTeams(runners) {
-        var playersSortableHTML = '' +
-            '<ul id="playerLayoutSortable">' +
-            playerLayout_CreateTeamListHtmlElements(runners) +
-            '</ul>';
-        $('#playerLayoutContainer').html(playersSortableHTML);
-        $("#playerLayoutSortable").sortable({
-            stop: function (event, ui) {
-                // IE doesn't register the blur when sorting
-                // so trigger focusout handlers to remove .ui-state-focus
-                var sortedIDs = $('#playerLayoutSortable').sortable("toArray");
-                var oldTeamArray = runDataActiveRunReplicant.value.teams;
-                var newTeamArray = [];
-                $.each(sortedIDs, function (index, valueId) {
-                    $.each(oldTeamArray, function (index, team) {
-                        if (team.name == valueId) {
-                            newTeamArray.push(team);
-                            return false;
-                        }
-                    });
-                });
-                runDataActiveRunReplicant.value.teams = newTeamArray;
-            }
-        });
-
-        $("#playerLayoutSortable").disableSelection();
-    }
-
-    function playerLayout_CreateTeamListHtmlElements(teamArray) {
-        var runnerHtml = '';
-        $.each(teamArray, function (index, value) {
-            runnerHtml += '<li class="ui-state-default" id="' + value.name + '">' + value.name + '</li>';
-        });
-        return runnerHtml;
-    }
-
-})
+$(function() {
+	var runDataActiveRunReplicant = nodecg.Replicant("runDataActiveRun");
+	runDataActiveRunReplicant.on("change", function (newValue, oldValue) {
+		if (typeof newValue !== 'undefined' && newValue != '') {
+			if (newValue.teams.length > 1)
+				playerLayout_UpdateLayoutPanelWithTeams(newValue.teams);
+			else if (newValue.teams.length === 1)
+				playerLayout_UpdateLayoutPanelWithPlayers(newValue.teams[0]);
+		}
+	});
+	
+	function playerLayout_UpdateLayoutPanelWithTeams(runners) {
+		var playersSortableHTML = '' +
+			'<ul id="playerLayoutSortable">' +
+			playerLayout_CreateTeamListHtmlElements(runners) +
+			'</ul>';
+		$('#playerLayoutContainer').html(playersSortableHTML);
+		$("#playerLayoutSortable").sortable({
+			stop: function (event, ui) {
+				// IE doesn't register the blur when sorting
+				// so trigger focusout handlers to remove .ui-state-focus
+				var sortedIDs = $('#playerLayoutSortable').sortable("toArray");
+				var oldTeamArray = runDataActiveRunReplicant.value.teams;
+				var newTeamArray = [];
+				$.each(sortedIDs, function (index, valueId) {
+					$.each(oldTeamArray, function (index, team) {
+						if (team.name == valueId) {
+							newTeamArray.push(team);
+							return false;
+						}
+					});
+				});
+				runDataActiveRunReplicant.value.teams = newTeamArray;
+			}
+		});
+		
+		$("#playerLayoutSortable").disableSelection();
+	}
+	
+	function playerLayout_CreateTeamListHtmlElements(teamArray) {
+		var runnerHtml = '';
+		$.each(teamArray, function (index, value) {
+			runnerHtml += '<li class="ui-state-default" id="' + value.name + '">' + value.name + '</li>';
+		});
+		return runnerHtml;
+	}
+	
+	function playerLayout_UpdateLayoutPanelWithPlayers(runners) {
+		var playersSortableHTML = '' +
+			'<ul id="playerLayoutSortable">' +
+			playerLayout_CreatePlayerListHtmlElements(runners) +
+			'</ul>';
+		$('#playerLayoutContainer').html(playersSortableHTML);
+		$("#playerLayoutSortable").sortable({
+			stop: function (event, ui) {
+				// IE doesn't register the blur when sorting
+				// so trigger focusout handlers to remove .ui-state-focus
+				var sortedIDs = $('#playerLayoutSortable').sortable("toArray");
+				var oldPlayerArray = runDataActiveRunReplicant.value.teams[0].members;
+				var newPlayerArray = [];
+				$.each(sortedIDs, function (index, valueId) {
+					$.each(oldPlayerArray, function (index, player) {
+						if (player.names.international == valueId) {
+							newPlayerArray.push(player);
+							return false;
+						}
+					});
+				});
+				runDataActiveRunReplicant.value.teams[0].members = newPlayerArray;
+			}
+		});
+		
+		$("#playerLayoutSortable").disableSelection();
+	}
+	
+	function playerLayout_CreatePlayerListHtmlElements(playerArray) {
+		var runnerHtml = '';
+		$.each(playerArray.members, function (index, value) {
+			runnerHtml += '<li class="ui-state-default" id="' + value.names.international + '">' + value.names.international + '</li>';
+		});
+		return runnerHtml;
+	}
+});
