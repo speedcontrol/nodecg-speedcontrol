b462a5d9d83126ef067e65af10a74c12eb95c743
zoton2 <zoton2@gmail.com>
2017-06-22 23:59:17 +0100

    Starting code to get player's region/country
    This tries to find the user on speedrun.com and if they can be found and
    have a country/region set, puts that string in the runData
    (runData.region); this can be used on layouts to put flags on them. Also
    some code cleanup/changing to accommodate this stuff.

--- dashboard/HoraroImportDashboard.js
+++ dashboard/HoraroImportDashboard.js
@@ -24,14 +24,10 @@ $(function () {
         $.ajax({
             url: jsonURL,
             dataType: "jsonp",
-            data: {
-                q: {}
-            },
-            success: function (data) {
-                console.log(data);
+            success: function(data) {
                 var runItems = data.schedule.items;
                 var itemCounter = 0;
-                runItems.forEach(function (run) {
+				async.eachSeries(runItems, function(run, callback) {
                     itemCounter++;
                     if(typeof run.data === 'undefined' ||
                        run.data == null ||
@@ -62,82 +58,99 @@ $(function () {
                        }
 
                     }
+					
                     var runData = createRunData();
 					
+					// Game Name
 					// Checking to see if the game name is a link, if not use the whole field.
 					if (run.data[0].match(/\((.*?)\)/)) 
-						var gameName = run.data[0].match(/\[(.*?)\]/)[1];
+						runData.game = run.data[0].match(/\[(.*?)\]/)[1];
 					else
-						var gameName = run.data[0];
+						runData.game = run.data[0];
 					
-                    runData.game = gameName;
+					// Estimate
                     runData.estimate = msToTime(run.length_t);
-                    if(run.data[3] != null) {
+					
+					// Category
+                    if(run.data[3] !== null)
                         runData.category = run.data[3];
-                    }
-                    else {
-                        runData.category ="";
-                    }
-
-                    if(run.data[2] != null) {
+					
+					// System
+                    if(run.data[2] !== null)
                         runData.system = run.data[2];
-                    }
-                    else {
-                        runData.system = "";
-                    }
-
-                    runData.region = "";
-
+					
+					// Teams/Players (there's a lot of stuff here!)
                     var playerLinksList = run.data[1];
-
-                    if (playerLinksList != null) {
-                        var vsList =playerLinksList.split(/vs\./);
-                        vsList.forEach( function( rawTeam, index) {
+					
+                    if (playerLinksList !== null) {
+						// Splitting by 'vs.' in case this is a race.
+                        var vsList = playerLinksList.split(/vs\./);
+						async.eachSeries(vsList, function(rawTeam, callback) {
+							// Getting/setting the team name.
                             var cap = rawTeam.match(/(Team\s*)?(\S+):\s+\[/);
-                            var teamName = "";
-                            if (cap !== null && cap.length > 0) {
-                              teamName = cap[2];
-                            }
-                            else {
-                              teamName = "Team "+(runData.teams.length+1);
-                            }
+                            if (cap !== null && cap.length > 0)
+								var teamName = cap[2];
+                            else
+								var teamName = "Team "+(runData.teams.length+1);
+							
+							// Getting the members of this team.
                             var members = rawTeam.split(",");
                             var team = {
-                              name: teamName,
-                              members: new Array()
+								name: teamName,
+								members: new Array()
                             };
-
-                            for (var i=0; i < members.length; i++) {
-
-                              if (members[i].match(/\((.*?)\)/)) {
-                                var username = members[i].match(/\((.*?)\)/)[1];
-                                var playerName = members[i].match(/\[(.*?)\]/)[1];
-                              }
-                              else {
-								  var playerName = members[i];
-                              }
-                              var member = {
-                                  names: {
-                                    international: playerName
-                                  },
-                                  twitch: {
-                                    uri: username
-                                  },
-                                  team: team.name
-                              };
-                              team.members.push(member);
-                              runData.players.push(member);
-                            }
-                            if (members.length == 1) {
-                              team.name = team.members[0].names.international;
-                              team.members[0].team = team.name;
-                            }
-                            runData.teams.push(team);
-                        });
+							
+							// Going through the list of members.
+							async.eachSeries(members, function(member, callback) {
+								// Checking to see if the user is a link, if not use the whole field.
+								if (member.match(/\((.*?)\)/)) {
+									var username = member.match(/\((.*?)\)/)[1];
+									var playerName = member.match(/\[(.*?)\]/)[1];
+								}
+								else
+									var playerName = member;
+								
+								getRegionFromSpeedrunCom(playerName, username, function(regionCode) {
+									// Creating the member object.
+									var memberObj = {
+										names: {
+											international: playerName
+										},
+										twitch: {
+											uri: username
+										},
+										team: team.name,
+										region: regionCode
+									};
+									
+									// Push this object to the relevant arrays where it is stored.
+									team.members.push(memberObj);
+									runData.players.push(memberObj);
+									callback();
+								});
+							}, function(err) {
+								// If there's only 1 member in the team, set the team name as their name.
+								if (members.length === 1) {
+									team.name = team.members[0].names.international;
+									team.members[0].team = team.name;
+								}
+								
+								runData.teams.push(team);
+								callback();
+							});
+                        }, function(err) {
+							horaro_AddRun(runData);
+							callback();
+						});
                     }
-                    horaro_AddRun(runData);
-                });
-                horaro_finalizeRunList();
+					
+					else {
+						horaro_AddRun(runData);
+						callback();
+					}
+                }, function(err) {
+					horaro_finalizeRunList();
+				});
             }
         });
 
@@ -157,7 +170,26 @@ $(function () {
         theRun.teams = new Array();
         return theRun;
     }
-
+	
+	// Tries to find the specified user on speedrun.com and get their country/region.
+	// Only using username lookups for now, need to use both in case 1 doesn't work.
+	function getRegionFromSpeedrunCom(username, twitch, callback) {
+		$.ajax({
+			url: 'http://www.speedrun.com/api/v1/users?max=1&lookup='+username.toLowerCase(),
+			dataType: "jsonp",
+			success: function(data) {
+				if (data.data.length > 0 && data.data[0].location) {
+					if (data.data[0].location.region)
+						callback(data.data[0].location.region.code);
+					else
+						callback(data.data[0].location.country.code);
+				}
+				
+				else callback();
+			}
+		});
+	}
+	
 // Called as a process when pushing the "add run" button
     function horaro_AddRun(runData) {
         runData.runID = runNumberIterator;
