386fe78db2e4a9567222e90360b0d099a7e8e123
zoton2 <zoton2@gmail.com>
2019-01-16 22:46:01 +0000

    Revert "Add ability to disable FFZ featured channels setting"
    This reverts commit 9651cab3fc13dcfaff1bb33e1eccb2b4951c8b9d.

--- extension/ffzws.js
+++ extension/ffzws.js
@@ -16,8 +16,7 @@ var twitchChannelName;
 if (nodecg.bundleConfig && nodecg.bundleConfig.twitch && nodecg.bundleConfig.twitch.enable && nodecg.bundleConfig.twitch.ffzIntegration) {
 	nodecg.log.info("FrankerFaceZ integration is enabled.");
 
-	if (!nodecg.bundleConfig.twitch.ffzUseRepeater)
-		nodecg.listenFor('updateFFZFollowing', setFFZFollowing);
+	nodecg.listenFor('updateFFZFollowing', setFFZFollowing);
 	
 	// Used to store whatever the WS says are the current buttons on the page.
 	ffzFollowButtonsReplicant = nodecg.Replicant('ffzFollowButtons', {persistent: false});
