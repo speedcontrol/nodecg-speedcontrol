a341dbe0041ee5f1a90923319942a84bbbe30800
zoton2 <zoton2@gmail.com>
2019-05-16 23:56:04 +0100

    Fix timer numbers not being Floor'd

--- src/extension/timer.ts
+++ src/extension/timer.ts
@@ -193,14 +193,14 @@ function resetStopwatchToDefault() {
   stopwatch.value = clone(defaultStopwatch);
 }
 
-function msToTime(duration: number) {
-  const seconds = (duration / 1000) % 60;
-  const minutes = (duration / (1000 * 60)) % 60;
-  const hours = (duration / (1000 * 60 * 60)) % 24;
+function msToTime(ms: number) {
+  const seconds = Math.floor((ms / 1000) % 60);
+  const minutes = Math.floor((ms / (1000 * 60)) % 60);
+  const hours = Math.floor(ms / (1000 * 60 * 60));
 
-  const hoursStr = (hours < 10) ? `0${hours}` : hours;
-  const minutesStr = (minutes < 10) ? `0${minutes}` : minutes;
-  const secondsStr = (seconds < 10) ? `0${seconds}` : seconds;
+  const hoursStr = (hours < 10) ? `0${hours}` : `${hours}`;
+  const minutesStr = (minutes < 10) ? `0${minutes}` : `${minutes}`;
+  const secondsStr = (seconds < 10) ? `0${seconds}` : `${seconds}`;
 
   return `${hoursStr}:${minutesStr}:${secondsStr}`;
 }
