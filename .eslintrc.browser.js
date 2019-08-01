3d7e87b28c9e2928e002835c13ba59952b2929bf
zoton2 <zoton2@gmail.com>
2019-08-01 23:43:23 +0100

    Revert "tsconfig.json is relative to project files?"
    This reverts commit d748bed6aa9c8fc76cb10318838d6a0316404f7f.

--- .eslintrc.browser.js
+++ .eslintrc.browser.js
@@ -3,7 +3,7 @@ module.exports = {
   parser: 'vue-eslint-parser',
   parserOptions: {
     parser: '@typescript-eslint/parser',
-    project: './tsconfig.json',
+    project: './tsconfig.browser.json',
   },
   env: {
     browser: true,
