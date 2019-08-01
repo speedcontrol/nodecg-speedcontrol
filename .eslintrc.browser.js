d748bed6aa9c8fc76cb10318838d6a0316404f7f
zoton2 <zoton2@gmail.com>
2019-08-01 23:32:54 +0100

    tsconfig.json is relative to project files?

--- .eslintrc.browser.js
+++ .eslintrc.browser.js
@@ -3,7 +3,7 @@ module.exports = {
   parser: 'vue-eslint-parser',
   parserOptions: {
     parser: '@typescript-eslint/parser',
-    project: './tsconfig.browser.json',
+    project: './tsconfig.json',
   },
   env: {
     browser: true,
