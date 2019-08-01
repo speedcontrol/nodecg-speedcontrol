257948b6b35a91b047e1d0f54fe6fdf07a833840
zoton2 <zoton2@gmail.com>
2019-08-02 00:40:46 +0100

    Renaming tweaks and remove graphics

--- script/bundle.js
+++ script/bundle.js
@@ -4,15 +4,6 @@ const command = process.env.NODE_ENV === 'production' ? 'build' : 'watch';
 
 fork('./node_modules/parcel-bundler/bin/cli.js', [
   command,
-  'src/graphics/*.html',
-  '--out-dir',
-  'graphics',
-  '--public-url',
-  '.',
-]);
-
-fork('./node_modules/parcel-bundler/bin/cli.js', [
-  command,
   'src/dashboard/*.html',
   '--out-dir',
   'dashboard',
