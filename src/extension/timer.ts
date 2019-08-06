510014778118d357dd522cdc3715dd3f7ccdc1d6
zoton2 <zoton2@gmail.com>
2019-08-06 12:52:51 +0100

    Move reused simple functions to helper file

--- src/extension/timer.ts
+++ src/extension/timer.ts
@@ -1,6 +1,7 @@
 import livesplitCore from 'livesplit-core';
 import { NodeCG, Replicant } from 'nodecg/types/server'; // eslint-disable-line
 import { Timer } from '../../types';
+import { msToTimeStr, timeStrToMS } from './util/helpers';
 import * as nodecgApiContext from './util/nodecg-api-context';
 
 // Cross references for LiveSplit's TimerPhases.
@@ -10,37 +11,6 @@ const LS_TIMER_PHASE = {
   Ended: 2,
   Paused: 3,
 };
-
-/**
- * Checks if number needs a 0 adding to the start and does so if needed.
- * @param num Number which you want to turn into a padded string.
- */
-function padTimeNumber(num: number): string {
-  return num.toString().padStart(2, '0');
-}
-
-/**
- * Converts milliseconds into a time string (HH:MM:SS).
- * @param ms Milliseconds you wish to convert.
- */
-function msToTimeStr(ms: number): string {
-  const seconds = Math.floor((ms / 1000) % 60);
-  const minutes = Math.floor((ms / (1000 * 60)) % 60);
-  const hours = Math.floor(ms / (1000 * 60 * 60));
-  return `${padTimeNumber(hours)}:${padTimeNumber(minutes)}:${padTimeNumber(seconds)}`;
-}
-
-/**
- * Converts a time string (HH:MM:SS) into milliseconds.
- * @param time Time string you wish to convert.
- */
-function timeStrToMS(time: string): number {
-  const ts = time.split(':');
-  if (ts.length === 2) {
-    ts.unshift('00'); // Adds 0 hours if they are not specified.
-  }
-  return Date.UTC(1970, 0, 1, Number(ts[0]), Number(ts[1]), Number(ts[2]));
-}
 
 export default class TimerApp {
   /* eslint-disable */
---
+++ src/extension/util/helpers.ts
@@ -0,0 +1,30 @@
+/**
+ * Checks if number needs a 0 adding to the start and does so if needed.
+ * @param num Number which you want to turn into a padded string.
+ */
+export function padTimeNumber(num: number): string {
+  return num.toString().padStart(2, '0');
+}
+
+/**
+ * Converts a time string (HH:MM:SS) into milliseconds.
+ * @param time Time string you wish to convert.
+ */
+export function timeStrToMS(time: string): number {
+  const ts = time.split(':');
+  if (ts.length === 2) {
+    ts.unshift('00'); // Adds 0 hours if they are not specified.
+  }
+  return Date.UTC(1970, 0, 1, Number(ts[0]), Number(ts[1]), Number(ts[2]));
+}
+
+/**
+ * Converts milliseconds into a time string (HH:MM:SS).
+ * @param ms Milliseconds you wish to convert.
+ */
+export function msToTimeStr(ms: number): string {
+  const seconds = Math.floor((ms / 1000) % 60);
+  const minutes = Math.floor((ms / (1000 * 60)) % 60);
+  const hours = Math.floor(ms / (1000 * 60 * 60));
+  return `${padTimeNumber(hours)}:${padTimeNumber(minutes)}:${padTimeNumber(seconds)}`;
+}
