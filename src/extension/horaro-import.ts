614ee9afae9c2e8a134a4cc02fd6dec6a20f109d
zoton2 <zoton2@gmail.com>
2019-08-09 19:30:49 +0100

    Horaro Import: config and stuff
    Added bundle config, added temp. cache for sr.com lookups, add ability to disable sr.com lookup, added checks for ignored games, added parsing of custom schedule data

--- src/extension/horaro-import.ts
+++ src/extension/horaro-import.ts
@@ -5,13 +5,18 @@ import { NodeCG, Replicant } from 'nodecg/types/server'; // eslint-disable-line
 import { mapSeries } from 'p-iteration';
 import removeMd from 'remove-markdown';
 import uuid from 'uuid/v4';
+import { Configschema } from '../../configschema';
 import { RunData, RunDataArray, RunDataPlayer, RunDataTeam } from '../../types'; // eslint-disable-line
 import Helpers from './util/helpers';
 
 const { msToTimeStr, nullToUndefined, sleep } = Helpers;
 const md = new MarkdownIt();
 let nodecg: NodeCG;
+let config: Configschema;
 let runDataArray: Replicant<RunDataArray>;
+const userDataCache: {
+  [k: string]: SRcomUserData;
+} = {};
 
 interface ParsedMarkdown {
   url?: string;
@@ -26,6 +31,19 @@ interface HoraroScheduleItem {
   data: (string | null)[];
   options: {
     setup?: string;
+  };
+}
+
+interface ColumnsOptions {
+  game: number;
+  gameTwitch: number;
+  category: number;
+  system: number;
+  region: number;
+  release: number;
+  player: number;
+  custom: {
+    [k: string]: number;
   };
 }
 
@@ -75,8 +93,10 @@ function parseMarkdown(str?: string): ParsedMarkdown {
  */
 function querySRcomUserData(str?: string): Promise<SRcomUserData> {
   return new Promise(async (resolve): Promise<void> => {
-    if (!str) {
+    if (!str || config.schedule.disableSpeedrunComLookup) {
       resolve();
+    } else if (userDataCache[str]) {
+      resolve(userDataCache[str]);
     } else {
       let success = true;
       do {
@@ -86,8 +106,10 @@ function querySRcomUserData(str?: string): Promise<SRcomUserData> {
             'get',
             encodeURI(`https://www.speedrun.com/api/v1/users?max=1&lookup=${str.toLowerCase()}`),
           );
+          await sleep(1000); // eslint-disable-line
           // @ts-ignore: parser exists but isn't in the typings
           if (resp.parser === 'json') {
+            [userDataCache[str]] = resp.body.data; // Simple temporary cache storage.
             resolve(resp.body.data[0]);
           } else {
             resolve();
@@ -132,7 +154,6 @@ function parseSRcomUserData(name?: string, twitchURL?: string): Promise<{
       foundData.twitchURL = (data.twitch && data.twitch.uri) ? data.twitch.uri : undefined;
     }
 
-    await sleep(1000);
     resolve(foundData);
   });
 }
@@ -145,11 +166,25 @@ function generateRunHash(colData: (string | null)[]): string {
   return crypto.createHash('sha1').update(colData.join(), 'utf8').digest('hex');
 }
 
+/**
+ * Checks if the game name appears in the ignore list in the configuration.
+ * @param game Game string (or null) to check against.
+ */
+function checkGameAgainstIgnoreList(game: string | null): boolean {
+  if (!game) {
+    return false;
+  }
+  const list = config.schedule.ignoreGamesWhileImporting || [];
+  return !!list.find((str): boolean => !!str.toLowerCase().match(
+    new RegExp(`\\b${game.toLowerCase()}\\b`),
+  ));
+}
+
 function parseSchedule(): Promise<RunDataArray> {
   return new Promise(async (resolve, reject): Promise<void> => {
     try {
       // Needs to be assigned somewhere else.
-      const columns = {
+      const columns: ColumnsOptions = {
         game: 0,
         gameTwitch: -1,
         category: 3,
@@ -157,13 +192,22 @@ function parseSchedule(): Promise<RunDataArray> {
         region: -1,
         release: -1,
         player: 1,
+        custom: {
+          layout: 5,
+        },
       };
 
-      const resp = await needle('get', 'https://horaro.org/esa/tceu19.json');
+      if (!config.schedule.defaultURL) {
+        throw new Error('Schedule URL is not defined.');
+      }
+      const resp = await needle('get', config.schedule.defaultURL);
       const runItems: HoraroScheduleItem[] = resp.body.schedule.items;
       const defaultSetupTime: number = resp.body.schedule.setup_t;
 
-      const newRunDataArray = await mapSeries(runItems, async (run, index): Promise<RunData> => {
+      // Filtering out any games on the ignore list before processing them all.
+      const newRunDataArray = await mapSeries(runItems.filter((run): boolean => (
+        !checkGameAgainstIgnoreList(run.data[columns.game])
+      )), async (run, index): Promise<RunData> => {
         // If a run with the same hash exists already, assume it's the same and use the same UUID.
         const hash = generateRunHash(run.data);
         const matchingOldRun = runDataArray.value.find((oldRun): boolean => oldRun.hash === hash);
@@ -189,10 +233,18 @@ function parseSchedule(): Promise<RunDataArray> {
         runData.estimateS = run.length_t;
         runData.estimate = msToTimeStr(run.length_t * 1000);
 
-        // Setup Time.
+        // Setup Time
         // (need to do custom setup times here as well)
         runData.setupTime = msToTimeStr(defaultSetupTime * 1000);
         runData.setupTimeS = defaultSetupTime;
+
+        // Custom Data
+        Object.keys(columns.custom).forEach((col): void => {
+          const { str } = parseMarkdown(nullToUndefined(run.data[columns.custom[col]]));
+          if (str) {
+            runData.customData[col] = str;
+          }
+        });
 
         // Players
         // (do we need this if? I like it for organisation at least)
@@ -270,6 +322,7 @@ export default class HoraroImport {
     nodecg = nodecg_;
     this.nodecg = nodecg;
     this.h = new Helpers(nodecg_);
+    config = this.h.bundleConfig();
     runDataArray = this.nodecg.Replicant('runDataArray');
     this.runDataArray = runDataArray;
 
@@ -278,8 +331,8 @@ export default class HoraroImport {
       parseSchedule().then((runs): void => {
         this.runDataArray.value = runs;
         this.nodecg.log.info('Successfully imported Horaro schedule.');
-      }).catch((): void => {
-        this.nodecg.log.warn('Error importing Horaro schedule.');
+      }).catch((err: Error): void => {
+        this.nodecg.log.warn('Error importing Horaro schedule:', err);
       });
     });
   }
