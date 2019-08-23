cf5eddfb8c418af3049703012dea5c7b3e20a745
zoton2 <zoton2@gmail.com>
2019-08-23 18:50:51 +0100

    Move Vuetify settings to new file, add to all dashboards
    This means a lot of them look a bit strange right now, but that will be fixed.

--- src/dashboard/player-layout/main.ts
+++ src/dashboard/player-layout/main.ts
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import * as replicantStore from '../_misc/replicant-store';
+import vuetify from '../_misc/vuetify';
 import App from './main.vue';
 
 replicantStore.create().then(() => {
   new Vue({
+    vuetify,
     el: '#App',
     render: h => h(App),
   });
--- src/dashboard/timer/main.ts
+++ src/dashboard/timer/main.ts
@@ -1,14 +1,11 @@
-import '@mdi/font/css/materialdesignicons.css';
 import Vue from 'vue';
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
 import * as replicantStore from '../_misc/replicant-store';
+import vuetify from '../_misc/vuetify';
 import App from './main.vue';
-
-Vue.use(Vuetify);
 
 replicantStore.create().then(() => {
   new Vue({
+    vuetify,
     el: '#App',
     render: h => h(App),
   });
