c1d890a2e9a94239b29f146caec54c2d343947e5
zoton2 <zoton2@gmail.com>
2019-09-11 16:14:38 +0100

    Remove express dep (as we're using NodeCG's)

--- .eslintrc.extension.js
+++ .eslintrc.extension.js
@@ -15,6 +15,10 @@ module.exports = {
     'plugin:import/typescript',
   ],
   rules: {
+    'import/no-extraneous-dependencies': ['error', {
+      devDependencies: true, // Some places have dev deps imported where eslint complains.
+      packageDir: ['.', '../..'], // Check for deps in NodeCG folder as well.
+    }],
     'no-async-promise-executor': 'off', // Should try and fix these instead.
     '@typescript-eslint/ban-ts-ignore': 'off', // Fix this stuff in the future hopefully.
     'indent': 'off', // Do I need this? (and one below).
