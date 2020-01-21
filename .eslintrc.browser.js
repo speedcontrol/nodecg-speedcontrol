725e5109baecf557eb724a1a86b3188066d3b1c7
zoton2 <zoton2@gmail.com>
2020-01-21 23:44:48 +0000

    Update eslint rules/gitignore

--- .eslintrc.browser.js
+++ .eslintrc.browser.js
@@ -27,17 +27,35 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:import/typescript',
   ],
+  settings: {
+    'import/resolver': {
+      node: {
+        moduleDirectory: [
+          'node_modules',
+          '../..',
+        ],
+      },
+    },
+  },
   rules: {
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'never',
+      jsx: 'never',
+      ts: 'never',
+      tsx: 'never',
+    }],
     'import/no-extraneous-dependencies': ['error', {
       devDependencies: true, // Some places have dev deps imported where eslint complains.
       packageDir: ['.', '../..'], // Check for deps in NodeCG folder as well.
     }],
+    'import/no-unresolved': [2, { caseSensitive: false }],
     'vue/html-self-closing': ['error', {
       html: {
         component: 'never', // Transpiler(?) has issues with self closing components.
       },
     }],
     'max-len': ["error", { "code": 100 }],
+    'lines-between-class-members': 'off',
     // I legit think the 5 things below are broken, might be a typescript-eslint issue.
     'vue/no-parsing-error': 'off',
     'vue/valid-v-on': 'off',
--- .eslintrc.extension.js
+++ .eslintrc.extension.js
@@ -14,11 +14,28 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:import/typescript',
   ],
+  settings: {
+    'import/resolver': {
+      node: {
+        moduleDirectory: [
+          'node_modules',
+          '../..',
+        ],
+      },
+    },
+  },
   rules: {
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'never',
+      jsx: 'never',
+      ts: 'never',
+      tsx: 'never',
+    }],
     'import/no-extraneous-dependencies': ['error', {
       devDependencies: true, // Some places have dev deps imported where eslint complains.
       packageDir: ['.', '../..'], // Check for deps in NodeCG folder as well.
     }],
+    'import/no-unresolved': [2, { caseSensitive: false }],
     'max-len': ["error", { "code": 100 }],
     'require-atomic-updates': 'off',
   },
