be2a0c35742877cf62cbe74b50fdfc8a111ce4e9
zoton2 <zoton2@gmail.com>
2020-03-13 00:10:18 +0000

    Remove currently unused oengusAPIData schema

--- schemas/index.d.ts
+++ schemas/index.d.ts
@@ -1,7 +1,6 @@
 export * from './defaultSetupTime';
 export * from './horaroImportSavedOpts';
 export * from './horaroImportStatus';
-export * from './oengusAPIData';
 export * from './runDataActiveRun';
 export * from './runDataActiveRunSurrounding';
 export * from './runDataArray';
