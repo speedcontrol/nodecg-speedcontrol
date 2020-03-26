3a0b3abb13e4e631400a12465bad547ec5fa023a
zoton2 <zoton2@gmail.com>
2020-03-26 03:45:22 +0000

    Modify/add some types/schemas

--- schemas/index.d.ts
+++ schemas/index.d.ts
@@ -1,6 +1,7 @@
 export * from './defaultSetupTime';
 export * from './horaroImportSavedOpts';
 export * from './horaroImportStatus';
+export * from './oengusImportStatus';
 export * from './runDataActiveRun';
 export * from './runDataActiveRunSurrounding';
 export * from './runDataArray';
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,8 @@
 export * from './Events';
 export * from './ExtensionReturn';
 export * from './HoraroImport';
+export * from './Oengus';
 export * from './RunData';
 export * from './Speedruncom';
 export * from './Timer';
 export * from './Twitch';
-
