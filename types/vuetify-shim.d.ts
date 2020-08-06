243830e0d82f534985a2cd8b44340d950243ae31
zoton2 <zoton2@gmail.com>
2020-08-06 17:54:35 +0100

    eslint improvements

--- types/vuetify-shim.d.ts
+++ types/vuetify-shim.d.ts
@@ -1,5 +1,6 @@
 declare module 'vuetify/es5/services/goto' {
   import Vue from 'vue';
 
-  export default function goTo<T extends string | number | HTMLElement | Vue>(target: T, options?: any): any;
+  export default function goTo<T extends string | number | HTMLElement | Vue>
+    (target: T, options?: any): any; // eslint-disable-line @typescript-eslint/no-explicit-any
 }
