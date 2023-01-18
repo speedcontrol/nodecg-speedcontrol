58c637780524cc0aa64d5837126fa0c70ccfe80f
Ewan Lyon <websites@ewanlyon.com>
2023-01-19 10:39:22 +1100

    remove named default

--- src/dashboard/_misc/replicant_store.ts
+++ src/dashboard/_misc/replicant_store.ts
@@ -1,7 +1,7 @@
 import { RunDataActiveRun, RunFinishTimes } from '@nodecg-speedcontrol/types';
 import type { DefaultSetupTime, HoraroImportSavedOpts, HoraroImportStatus, OengusImportStatus, RunDataActiveRunSurrounding, RunDataArray, Timer, TimerChangesDisabled, TwitchAPIData, TwitchChannelInfo, TwitchCommercialTimer } from '@nodecg-speedcontrol/types/schemas';
 import clone from 'clone';
-import { default as NodeCGTypes } from '@alvancamp/test-nodecg-types'; // eslint-disable-lint import/no-named-default
+import type NodeCGTypes from '@alvancamp/test-nodecg-types';
 import Vue from 'vue';
 import type { Store } from 'vuex';
 import { namespace } from 'vuex-class';
